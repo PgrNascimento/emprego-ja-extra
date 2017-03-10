842cf471078bf7d25f97804f38c7dde5acb96ea4
Alan <alanoliveiran45@gmail.com>
2017-03-10 12:39:07 -0300

    User create category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,24 @@
+class CategoriesController < ApplicationController
+  def show
+    @category = Category.find params[:id]
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to @category
+    else
+      flash[:error] = "Não foi possível criar a categoria"
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)    
+  end
+end
---
+++ spec/features/user_create_categories_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User create categories' do
+  scenario 'successfully' do
+    category = build(:category, name: 'Desenvolvedor')
+
+    visit new_category_path
+
+    fill_in 'Nome', with: category.name
+
+    click_on 'Criar Categoria'
+
+    expect(page).to have_css('h1', text: category.name)
+  end
+  scenario 'and should fill all fields' do
+    visit new_category_path
+
+    click_on 'Criar Categoria'
+
+    expect(page).to have_content 'Não foi possível criar a categoria'
+  end
+end
